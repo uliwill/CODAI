a7df30cf19e42fab0c54702bdea61db5a37f3c93
Ulisses Willian Reichenbach <uliwill@gmail.com>
2021-05-09 01:49:19 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var nome = "Ulisses Willian Reichenbach";
+var cargo = "Desenvolvedor Front End";
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome) {
+  nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+  cargoHtml.innerHTML = cargo;
+}
+
+function clickNoSobre() {
+  texto1.style.display = "block";
+  texto2.style.display = "none";
+}
+
+function clickNoProjetos() {
+  texto2.style.display = "block";
+  texto1.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
